8ef6634ebb2255817c36d3e2aa7f12217b2e4de9
Ninosaurier <provenzano.a@gmx.net>
2024-04-21 11:54:56 +0200

    Removed docs example

---
+++ src/core/RootChart.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let {
+		heightViewbox = 150,
+		widthViewBox = 300,
+		width = 100,
+		height = 100, 
+	} = $props();
+</script>
+
+<svg viewBox="0 0 {widthViewBox} {heightViewbox}"  class="svg">
+	<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"> Hello World </text>
+</svg>
+
+<style>
+	.svg {
+		border: 2px solid black;
+	}
+</style>
