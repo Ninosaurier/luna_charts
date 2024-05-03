80f9f6e9489ac0f495d78dfa5da8ed2a9f2f3347
Ninosaurier <provenzano.a@gmx.net>
2024-05-03 17:02:31 +0200

    moved tests

--- tests/core/Wrapper.test.ts
+++ tests/core/Wrapper.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import RootChart from './Wrapper.svelte';
+import RootChart from '../../src/core/Wrapper.svelte';
 import { mount } from 'svelte';
 
 
